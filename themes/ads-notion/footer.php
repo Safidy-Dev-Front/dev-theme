bee713a31d643aaa4b378bec2a7aa881172ddf0b
Safidy-Dev-Front <safidiniainaromino13@gmail.com>
2022-03-02 21:37:56 +0300

    header-footer

--- themes/ads-notion/footer.php
+++ themes/ads-notion/footer.php
@@ -1,5 +1,21 @@
-</main>
-<footer>
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+<footer>
+    
 </footer>
 <?php wp_footer(); ?>
+
+</body>
+
+</html>
+
